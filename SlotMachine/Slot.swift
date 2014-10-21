68ffaf0e7bae657de142e298c3c47f0ed9e716d1
mhhonline <mhhonline@gmail.com>
2014-10-21 22:44:49 +0300

    till SlotMachine: Check for Flush

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Mohammed Alhanabi on 12/27/1435 AH.
+//  Copyright (c) 1435 nightwaresystems.com. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
---
+++ SlotMachine/SlotBrain.swift
@@ -0,0 +1,74 @@
+//
+//  SlotBrain.swift
+//  SlotMachine
+//
+//  Created by Mohammed Alhanabi on 12/27/1435 AH.
+//  Copyright (c) 1435 nightwaresystems.com. All rights reserved.
+//
+
+import Foundation
+
+class SlotBrain {
+    class func unpackSlotsIntoSlotRows (slots: [[Slot]]) -> [[Slot]] {
+        var slotRow: [Slot] = []
+        var slotRow2: [Slot] = []
+        var slotRow3: [Slot] = []
+        for slotArray in slots {
+            for var index = 0; index < slotArray.count; index++ {
+                let slot = slotArray[index]
+                if index == 0 {
+                    slotRow.append(slot)
+                }
+                else if index == 1 {
+                    slotRow2.append(slot)
+                }
+                else if index == 2 {
+                    slotRow3.append(slot)
+                }
+                else {
+                    println("Error")
+                }
+            }
+        }
+        var slotInRows: [[Slot]] = [slotRow, slotRow2, slotRow3]
+        return slotInRows
+    }
+    
+    class func computeWinnings (slots: [[Slot]]) -> Int {
+        var slotsInRows = unpackSlotsIntoSlotRows(slots)
+        var winnings = 0
+        var flushWinCount = 0
+        var threeOfAKindWinCount = 0
+        var straightWinCount = 0
+        for slotRow in slotsInRows {
+            if checkFlush(slotRow) == true
+            {
+                println("flush")
+                winnings += 1
+                flushWinCount += 1
+            }
+        }
+        if flushWinCount == 3 {
+            println("Royal Flush")
+            winnings += 15
+        }
+
+        return winnings
+    }
+    
+    class func checkFlush (slotRow: [Slot]) -> Bool {
+        let slot1 = slotRow[0]
+        let slot2 = slotRow[1]
+        let slot3 = slotRow[2]
+        if slot1.isRed == true && slot2.isRed == true && slot3.isRed == true {
+            return true
+        }
+        else if slot1.isRed == false && slot2.isRed == false && slot3.isRed == false {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+}
+
